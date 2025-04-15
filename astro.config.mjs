58f1cbdc1080ff4de2cad862582f10b6e7660bd6
hudamnhd <muhamadhuda519@gmail.com>
2025-04-15 21:18:41 +0700

    fix build error sharp not found

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,141 +1,124 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    locales: ["en", "id"],
-    defaultLocale: "en",
-  },
-  integrations: [
-    starlight({
-      plugins: [
-        starlightBlog({
-          authors: {
-            huda: {
-              name: "Huda",
-              title: "Sofware Developer",
-              picture: "https://avatars.githubusercontent.com/u/127742560?s=200",
-              url: "https://hudamnhd.vercel.app"
-            },
-          },
-        })],
-      editLink: {
-        baseUrl: "https://github.com/HiDeoo/starlight-blog/edit/main/docs/",
-      },
-      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 6 },
-      title: "Huda",
-      defaultLocale: 'en',
-      lastUpdated: true,
-      customCss: ["./src/styles/custom.css"],
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/withastro/starlight",
-        },
-      ],
-      sidebar: [
-        {
-          label: "About Me",
-          translations: {
-            'id': 'Tentang Saya',
-          },
-          link: "/about-me",
-        },
-        // A group of links labelled "Learning".
-        {
-          label: 'Learn',
-          collapsed: false,
-          translations: {
-            'id': 'Belajar',
-          },
-          items: [
-            {
-              label: 'Linux',
-              collapsed: false,
-              items: [
-                'learning/linux/fundamental',
-                'learning/linux/awesome-linux-apps',
-              ],
-            },
-            {
-              label: 'HTML',
-              collapsed: false,
-              items: [
-                'learning/html/fundamental',
-              ],
-            },
-            {
-              label: 'CSS',
-              items: [
-                'learning/css/fundamental',
-              ],
-            },
-            {
-              label: 'Tailwind CSS',
-              items: [
-                'learning/tailwindcss/fundamental',
-              ],
-            },
-            {
-              label: 'Javascript',
-              items: [
-                'learning/javascript/fundamental',
-              ],
-            },
-            {
-              label: 'Typescript',
-              items: [
-                'learning/typescript/fundamental',
-              ],
-            },
-            {
-              label: 'Node JS',
-              items: [
-                'learning/nodejs/fundamental',
-              ],
-            },
-            {
-              label: 'React JS',
-              items: [
-                'learning/reactjs/fundamental',
-              ],
-            },
-            {
-              label: 'Remix',
-              items: [
-                'learning/remixjs/fundamental',
-              ],
-            },
-            {
-              label: 'Astro',
-              items: [
-                'learning/astro/fundamental',
-              ],
-            },
-            {
-              label: 'My Sql',
-              items: [
-                'learning/mysql/fundamental',
-              ],
-            },
-            {
-              label: 'Prisma',
-              items: [
-                'learning/prisma/fundamental',
-              ],
-            },
-          ],
-        },
-        {
-          label: "Tutorial",
-          autogenerate: { directory: "tutorial" },
-        },
-      ],
-      credits: true,
-    }),
-  ],
+	i18n: {
+		locales: ["en", "id"],
+		defaultLocale: "en",
+	},
+	integrations: [
+		starlight({
+			plugins: [
+				starlightBlog({
+					authors: {
+						huda: {
+							name: "Huda",
+							title: "Sofware Developer",
+							picture:
+								"https://avatars.githubusercontent.com/u/127742560?s=200",
+							url: "https://hudamnhd.vercel.app",
+						},
+					},
+				}),
+			],
+			editLink: {
+				baseUrl: "https://github.com/HiDeoo/starlight-blog/edit/main/docs/",
+			},
+			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 6 },
+			title: "Huda",
+			defaultLocale: "en",
+			lastUpdated: true,
+			customCss: ["./src/styles/custom.css"],
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/withastro/starlight",
+				},
+			],
+			sidebar: [
+				{
+					label: "About Me",
+					translations: {
+						id: "Tentang Saya",
+					},
+					link: "/about-me",
+				},
+				// A group of links labelled "Learning".
+				{
+					label: "Learn",
+					collapsed: false,
+					translations: {
+						id: "Belajar",
+					},
+					items: [
+						{
+							label: "Linux",
+							collapsed: false,
+							items: [
+								"learning/linux/fundamental",
+								"learning/linux/awesome-linux-apps",
+							],
+						},
+						{
+							label: "HTML",
+							collapsed: false,
+							items: ["learning/html/fundamental"],
+						},
+						{
+							label: "CSS",
+							items: ["learning/css/fundamental"],
+						},
+						{
+							label: "Tailwind CSS",
+							items: ["learning/tailwindcss/fundamental"],
+						},
+						{
+							label: "Javascript",
+							items: ["learning/javascript/fundamental"],
+						},
+						{
+							label: "Typescript",
+							items: ["learning/typescript/fundamental"],
+						},
+						{
+							label: "Node JS",
+							items: ["learning/nodejs/fundamental"],
+						},
+						{
+							label: "React JS",
+							items: ["learning/reactjs/fundamental"],
+						},
+						{
+							label: "Remix",
+							items: ["learning/remixjs/fundamental"],
+						},
+						{
+							label: "Astro",
+							items: ["learning/astro/fundamental"],
+						},
+						{
+							label: "My Sql",
+							items: ["learning/mysql/fundamental"],
+						},
+						{
+							label: "Prisma",
+							items: ["learning/prisma/fundamental"],
+						},
+					],
+				},
+				{
+					label: "Tutorial",
+					autogenerate: { directory: "tutorial" },
+				},
+			],
+			credits: true,
+		}),
+	],
+	image: {
+		service: passthroughImageService(),
+	},
 });
